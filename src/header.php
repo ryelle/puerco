f19db052b8906127581b957d036a72aae7bcd4ad
ryelle <kelly.dwan@gmail.com>
2014-10-23 14:14:14 -0400

    Style the site header. Description should only appear on the front page and home page (blog posts- and only on the first page of blog posts)

--- src/header.php
+++ src/header.php
@@ -23,15 +23,20 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'puerco' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
-
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle"><?php _e( 'Primary Menu', 'puerco' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
+
+		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+			<?php if ( get_bloginfo( 'description' ) ) : ?>
+				<?php if ( is_front_page() || ( is_home() && ! is_paged() ) ) : ?>
+					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+				<?php endif; ?>
+			<?php endif; ?>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
