5ed5fea361a61d1a802e2cb3c46f726fa117493a
ryelle <kelly.dwan@gmail.com>
2014-11-01 17:50:11 -0400

    Remove the description from the blog post page

--- src/header.php
+++ src/header.php
@@ -32,7 +32,7 @@
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
 			<?php if ( get_bloginfo( 'description' ) ) : ?>
-				<?php if ( is_front_page() || ( is_home() && ! is_paged() ) ) : ?>
+				<?php if ( is_front_page() ) : ?>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				<?php endif; ?>
 			<?php endif; ?>
