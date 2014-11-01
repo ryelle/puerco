5e7d47a049373a3d9a1c0856860b986c44cce999
ryelle <kelly.dwan@gmail.com>
2014-11-01 17:30:16 -0400

    Remove extra data from the list view

--- src/content.php
+++ src/content.php
@@ -7,27 +7,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php puerco_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php the_excerpt(); ?>
 		<?php puerco_continue_reading_link(); ?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'puerco' ),
-				'after'  => '</div>',
-			) );
-		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php puerco_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
