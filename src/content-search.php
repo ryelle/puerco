5c102cc05c9679d10fbc8e98afcf96d3476f7216
ryelle <kelly.dwan@gmail.com>
2014-11-02 14:08:04 -0500

    Use the index view for search pages

--- src/content-search.php
+++ src/content-search.php
@@ -11,19 +11,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php puerco_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div class="entry-content">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-
-	<footer class="entry-footer">
-		<?php puerco_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+		<?php puerco_continue_reading_link(); ?>
+	</div><!-- .entry-content -->
 </article><!-- #post-## -->
