1691de1f051f169235acb162aa2555e82bf051fa
ryelle <kelly.dwan@gmail.com>
2014-11-07 23:03:38 -0500

    Use JS to restructure the markup of the menu, and show/hide sections on click

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,6 +5,43 @@
  */
 ( function( $, undefined ) {
 
+	var $div,
+		container    = document.getElementById('menu-container'),
+		sections     = container.querySelectorAll('.menu-items'),
+		descriptions = [],
+		headers      = [],
+		functions    = {};
 
+	functions.show = function( index ) {
+		$( sections ).hide();
+		$( sections[ index ] ).show();
+		$( descriptions ).hide();
+		$( descriptions[ index ] ).show();
+
+		$( ".selected" ).removeClass('selected');
+		$( headers[index] ).addClass('selected');
+	}
+
+	$( sections ).each(function( index, element ) {
+		var header  = element.querySelector('.menu-group-header'),
+			descrip = element.querySelector('.menu-group-description');
+		$( element ).data( 'header', index );
+		$( header ).data( 'section', index );
+		$( descrip ).attr( 'data-section', index );
+		headers.push( header );
+		descriptions.push( descrip );
+	});
+
+	$( headers ).on( 'click', function(event){
+		event.preventDefault();
+		functions.show( $( this ).data('section') );
+	})
+
+	$div = $( '<div />' ).addClass('description-container');
+	$div.html( $( descriptions ).detach() ).prependTo( container );
+
+	$div = $( '<div />' ).addClass('header-container');
+	$div.html( $( headers ).detach() ).prependTo( container );
+	functions.show(0);
 
 } )( jQuery );
