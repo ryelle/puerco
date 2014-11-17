3dda4a982be8238525a7780d97c005624d691b0e
ryelle <kelly.dwan@gmail.com>
2014-11-17 14:50:20 -0500

    Move shared post & page styles into index, and add the post thumbnail to pages

--- src/content-page.php
+++ src/content-page.php
@@ -11,6 +11,12 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
+	<?php if ( has_post_thumbnail() ) : ?>
+	<div class="entry-image">
+		<?php the_post_thumbnail(); ?>
+	</div>
+	<?php endif; ?>
+
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
